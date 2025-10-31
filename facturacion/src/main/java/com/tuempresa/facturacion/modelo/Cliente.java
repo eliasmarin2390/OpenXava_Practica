8c65897e388440c08184f273fb1d6b3224a7391d
eliasmarin2390 <eamarin@uamv.edu.ni>
2025-10-30 18:28:30 -0600

    Subo lasegunda parte

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
@@ -2,13 +2,12 @@ package com.tuempresa.facturacion.modelo;
 
 import javax.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.openxava.annotations.*;
 
+import lombok.*;
+
 @Entity  // Esto marca la clase Cliente como una entidad
-@Getter
-@Setter // Esto hace los campos a continuación públicamente accesibles
+@Getter @Setter // Esto hace los campos a continuaci?n p?blicamente accesibles
 public class Cliente {
 
     @Id  // La propiedad numero es la clave.  Las claves son obligatorias (required) por defecto
@@ -16,7 +15,10 @@ public class Cliente {
     int numero;
 
     @Column(length=50) // La longitud de columna se usa a nivel UI y a nivel DB
-    @Required  // Se mostrará un error de validación si la propiedad nombre se deja en blanco
+    @Required  // Se mostrar? un error de validaci?n si la propiedad nombre se deja en blanco
     String nombre;
+
+    @Embedded @NoFrame // Así para referenciar a una clase incrustable
+    Direccion direccion; // Una referencia Java convencional
 
 }
--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Producto.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Producto.java
@@ -1,5 +1,11 @@
+package com.tuempresa.facturacion.modelo;
+
+import java.math.*;
+
 import javax.persistence.*;
+
 import org.openxava.annotations.*;
+
 import lombok.*;
 
 @Entity @Getter @Setter
@@ -10,5 +16,21 @@ public class Producto {
 
     @Column(length=50) @Required
     String descripcion;
+
+    @ManyToOne( // La referencia se almacena como una relaci?n en la base de datos
+            fetch=FetchType.LAZY, // La referencia se carga bajo demanda
+            optional=true) // La referencia puede estar sin valor
+    @DescriptionsList // As? la referencia se visualiza usando un combo
+    com.tuempresa.facturacion.modelo.Categoria categoria; // Una referencia Java convencional
+
+    @Money // La propiedad precio se usa para almacenar dinero
+    BigDecimal precio; // BigDecimal se suele usar para dinero
+
+    @Files // Una galer?a de fotos completa est? disponible
+    @Column(length=32) // La cadena de 32 de longitud es para almacenar la clave de la galer?a
+    String fotos;
+
+    @TextArea // Esto es para un texto grande, se usar? un ?rea de texto o equivalente
+    String observaciones;
 
 }
